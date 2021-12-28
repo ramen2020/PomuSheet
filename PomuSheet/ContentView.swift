2166d44fe25179f0a68b3ac24a592c2f1ac1d236
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-28 16:43:57 +0900

    init

---
+++ PomuSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomuSheet
+//
+//  Created by 宮本光直 on 2021/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomuSheet/PomuSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomuSheetApp.swift
+//  PomuSheet
+//
+//  Created by 宮本光直 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct PomuSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
