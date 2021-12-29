b628c08da39ce39542a8d716a412f83b8ba4298e
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-29 21:01:01 +0900

    add ExampleView file

---
+++ PomuSheet/Example/ExampleView.swift
@@ -0,0 +1,20 @@
+//
+//  ExampleView.swift
+//  PomuSheet
+//
+//  Created by 宮本光直 on 2021/12/28.
+//
+
+import SwiftUI
+
+struct ExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExampleView()
+    }
+}
